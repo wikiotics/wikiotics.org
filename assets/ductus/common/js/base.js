751ebcff208f86d4b04ef113304876bc817714f0
Jim Garrison <garrison@wikiotics.org>
2020-06-30 22:31:29 -0400

    Put lesson media in subdirs according to first two chars of hash

--- assets/ductus/common/js/base.js
+++ assets/ductus/common/js/base.js
@@ -4,5 +4,5 @@ function resolve_mediacache_url(resource, mime_type, additional_args, blob_urn) 
     var digest = split_urn[2];
     var dotstr = additional_args ? '.' + additional_args : '';
     var mime_ext = ductus_mime_to_ext[mime_type || resource.mime_type];
-    return ductus_mediacache_prefix + hash_type + '/' + digest + dotstr + '.' + mime_ext + '?' + resource.href;
+    return ductus_mediacache_prefix + hash_type + '/' + digest.substr(0, 2) + '/' + digest + dotstr + '.' + mime_ext + '?' + resource.href;
 }
